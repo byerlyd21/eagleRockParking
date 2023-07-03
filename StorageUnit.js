6ee4039aaf565de7844826191a9f1d8e1fb9edb2
Dallin Byerly <dallinbyerly@gmail.com>
2023-07-03 11:08:53 -0400

    Updated imports for firebase

--- StorageUnit.js
+++ StorageUnit.js
@@ -9,6 +9,19 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
+import { getDatabase, ref, set } from "firebase/database";
+
+const database = getDatabase();
+
+
+// function writeUserData(userId, name, email, imageUrl) {
+//   const db = getDatabase();
+//   set(ref(db, 'users/' + userId), {
+//     username: name,
+//     email: email,
+//     profile_picture : imageUrl
+//   });
+// }
 
 //Objects
 
