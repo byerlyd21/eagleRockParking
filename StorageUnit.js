c10241338090b3dc15229d3e1686b03ae8b70126
Dallin Byerly <dallinbyerly@gmail.com>
2023-07-03 12:11:50 -0400

    Update

--- StorageUnit.js
+++ StorageUnit.js
@@ -1,11 +1,7 @@
 import { initializeApp } from 'firebase/app';
 
 // TODO: Replace the following with your app's Firebase project configuration
-const firebaseConfig = {
-  //...
 
-  const app = initializeApp(firebaseConfig);
-};
 
 const app = initializeApp(firebaseConfig);
 
