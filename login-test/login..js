d54583e95268ca037166dae765062e513370c9c1
Dallin Byerly <dallinbyerly@gmail.com>
2023-07-08 10:49:08 -0400

    connected default password/ username for testing

--- login-test/login..js
+++ login-test/login..js
@@ -1,18 +1,20 @@
 const loginBtn = document.getElementById("login-btn");
 const registerBtn = document.getElementById("register");
 
-loginBtn.addEventListener("click", loginUser);
+loginBtn.addEventListener("click", (e) => {
+
+    if (userId.value === "DByerly" && password.value === "1234") {
+        window.location.href = "../src/StorageUnit.html"
+        e.preventDefault();
+    }
+});
+
 registerBtn.addEventListener("click", registerUser);
 console.log("hi");
 
 const userId = document.getElementById("user-id")
 const password = document.getElementById("password")
 
-function loginUser() {
-
-    console.log(userId.value, password.value)
-
-}
 
 function registerUser() {
 
