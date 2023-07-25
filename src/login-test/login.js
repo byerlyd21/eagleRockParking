3bb9280679434413f55252509b94f30e34442d0e
Dallin Byerly <dallinbyerly@gmail.com>
2023-07-25 12:54:55 -0400

    added webpack

--- src/login-test/login.js
+++ src/login-test/login.js
@@ -2,32 +2,47 @@
 //----------
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
-import { getDatabase, ref, set, child, get } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-database.js";
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
+// import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app-auth.js";
+// import { getDatabase, ref, set, child, get } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-database.js";
+import { initializeApp } from "firebase/app";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getDatabase, ref, set, child, get } from "firebase/database";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: "eagle-rock-parking.firebaseapp.com",
-  databaseURL: "https://eagle-rock-parking-default-rtdb.firebaseio.com",
-  projectId: "eagle-rock-parking",
-  storageBucket: "eagle-rock-parking.appspot.com",
-  messagingSenderId: process.env.SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.G-MEASUREMENT_ID
+    apiKey: "AIzaSyBCwWpAc4IndijYAxoSE5LDMy4orL__AE8",
+    authDomain: "eagle-rock-parking.firebaseapp.com",
+    databaseURL: "https://eagle-rock-parking-default-rtdb.firebaseio.com",
+    projectId: "eagle-rock-parking",
+    storageBucket: "eagle-rock-parking.appspot.com",
+    messagingSenderId: "123297692040",
+    appId: "1:123297692040:web:a013be1a203965a2e6e23d",
+    measurementId: "G-Y68B995K83"
 };
 
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
-// Initialize Realtime Database and get a reference to the service
+const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
+const auth = getAuth(app);
+
+
+// Detect auth state
+
+onAuthStateChanged(aut, (user)=> {
+    if (!user == null) {
+        console.log("logged in!")
+    } else {
+        console.log("no user")
+    }
+});
 
 // Login  --------------------------------------
 
@@ -72,16 +87,20 @@ const registerPassword = document.getElementById("register-password")
 
 const registerBtn = document.getElementById("register");
 
+function isEmptyOrSpaces(str) {
+    return str === null || str.match(/^ *$/) !== null;
+}
+
 function validation() {
     console.log("validating");
     let nameregx = /^[a-zA-Z]+$/;
     let email = /^[a-zA-Z0-9]+@\.(gmail|yahoo|outlook)\.com$/;
     let userregx = /^[a-zA-Z0-9]{5,}$/;
     
-    if (isEmptyOrSpaces(registerLocation.value) || isEmptyOrSpaces(registerEmail.value) || isEmptyOrSpaces(registerUserId.value) || isEmptyOrSpaces(registerPassword.value )) {
-        alert("All fields must be completed");
-        return false;
-    }
+    // if (isEmptyOrSpaces(registerLocation.value) || isEmptyOrSpaces(registerEmail.value) || isEmptyOrSpaces(registerUserId.value) || isEmptyOrSpaces(registerPassword.value )) {
+    //     alert("All fields must be completed");
+    //     return false;
+    // }
 
     if (!nameregx.test(registerLocation.value)) {
         alert("The name should only contain letters");
@@ -95,7 +114,7 @@ function validation() {
         alert("-username can only be alphanumeric\n-username must be at least 5 characters\n-username cannot contain spaces");
         return false;
     }
-
+    console.log("true")
     return true;
 }
 
